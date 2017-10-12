a9cadb79583eec6dab43ffc44f44b28c8f162b54
gaortiz1 <bisbiridolfo@hotmail.com>
2017-10-11 22:02:29 -0500

    remove_test

--- eye-connector/eye-connector-neo4j/src/test/java/com/eyebox/neo4j/config/TestNeo4jConfig.java
+++
@@ -1,9 +0,0 @@
-package com.eyebox.neo4j.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = {"com.eyebox.neo4j.repositories"})
-public class TestNeo4jConfig {
-}
